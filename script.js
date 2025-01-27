376f620e35962a02669ba2a74d43bb18be991407
juanda <juandacorreo@gmail.com>
2025-01-27 14:26:07 +0100

    implementado localStorage y jugar de nuevo

--- script.js
+++ script.js
@@ -3,6 +3,15 @@ const MAX_NUMBER = 20
 
 let number, highscore, score
 
+/* seleccionar todos los elementos del DOM que necesitamos */
+const messageField = document.querySelector('.message')
+const scoreField = document.querySelector('.score')
+const highscoreField = document.querySelector('.highscore')
+const numberField = document.querySelector('.number')
+const guessField = document.querySelector('.guess')
+const checkBtn = document.querySelector('.check')
+const againBtn = document.querySelector('.again')
+
 // el estado de mi aplicación se basa en:
 // -number: número aleatorio
 // score
@@ -13,21 +22,37 @@ initData()
 
 function initData() {
   score = INITIAL_SCORE
-  highscore = 0
+  scoreField.textContent = score
+  highscore = highscore || 0
   number = Math.trunc(Math.random() * MAX_NUMBER) + 1
   console.log(number, '*****************************')
 }
 
-/* seleccionar todos los elementos del DOM que necesitamos */
-const messageField = document.querySelector('.message')
-const scoreField = document.querySelector('.score')
-const highscoreField = document.querySelector('.highscore')
-const numberField = document.querySelector('.number')
-const guessField = document.querySelector('.guess')
-const checkBtn = document.querySelector('.check')
-const againBtn = document.querySelector('.again')
-
 checkBtn.addEventListener('click', checkNumber)
+
+againBtn.addEventListener('click', playAgain)
+
+// si pulsamos la tecla ENTER debería pulsar el botón checkButton
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Enter') {
+    checkNumber()
+  }
+})
+
+function playAgain() {
+  initData()
+  numberField.textContent = '?'
+  numberField.style.width = '15rem'
+  numberField.style.fontSize = '6rem'
+  numberField.style.backgroundColor = 'white'
+  document.body.style.backgroundColor = '#222'
+  checkBtn.disabled = false
+  // actualizo el campo highscore y highscoreField con el localStorage
+  // si no hay highscore en el localStorage, highscore es 0
+  // highscore tiene que ser number!!!
+  highscore = Number(localStorage.getItem('highscore')) || 0
+  highscoreField.textContent = highscore
+}
 
 function checkNumber() {
   // obtenemos el número pulsado
@@ -44,6 +69,12 @@ function checkNumber() {
     numberField.style.backgroundColor = 'red'
     document.body.style.backgroundColor = 'green'
     checkBtn.disabled = true
+    if (score > highscore) {
+      highscore = score
+      highscoreField.textContent = highscore
+      // guardamos el valor en el localStorage
+      localStorage.setItem('highscore', highscore)
+    }
   } else {
     if (score > 1) {
       const message =
